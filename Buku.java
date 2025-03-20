23f6587e39d85776632c699c31f208b0a55fec9c
aulialuluk <aulialuluk321@gmail.com>
2025-03-20 22:59:43 +0700

    Update Buku.java

--- Buku.java
+++ Buku.java
@@ -2,16 +2,28 @@ public class Buku {
     private String Judul;
     private String Penulis;
     private int TahunTerbit;
-  
-    public Buku(String Judul, String Penulis, int TahunTerbit){
+
+    public Buku(String Judul, String Penulis, int TahunTerbit) {
         this.Judul = Judul;
         this.Penulis = Penulis;
         this.TahunTerbit = TahunTerbit;
     }
-        public void tampilInfoBuku() {
-            System.out.println("Judul Buku: " + Judul);
-            System.out.println("Penulis Buku: " + Penulis);
-            System.out.println("Tahun Terbit: "+ TahunTerbit);
+
+    public void tampilInfoBuku() {
+        System.out.println("Judul Buku: " + Judul);
+        System.out.println("Penulis Buku: " + Penulis);
+        System.out.println("Tahun Terbit: " + TahunTerbit);
+    }
+
+    public String getJudul() {
+        return Judul;
+    }
+
+    public String getPenulis() {
+        return Penulis;
+    }
+
+    public int getTahunTerbit() {
+        return TahunTerbit;
     }
 }
-
